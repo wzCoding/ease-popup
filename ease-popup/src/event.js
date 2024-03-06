86e0fe82153a4f85f78f566021f654d03c90cc5b
wzCoding <1429354968@qq.com>
2024-03-06 15:48:57 +0800

    更新文件结构及提交代码

---
+++ ease-popup/src/event.js
@@ -0,0 +1,40 @@
+const triggerEvents = {   //触发事件选项
+    'click': ['click'],
+    'hover': ['mouseenter', 'mouseleave']
+}
+
+const allEvents = {
+    'scroll': ['scroll']
+}
+
+function EventListener(options = {
+    trigger: 'click',
+    element: null,
+    handler: null,
+    capture: false,
+    removeFlag: false
+}) {
+    const { trigger, element, handler, capture, removeFlag } = options
+    const events = triggerEvents[trigger] ? triggerEvents[trigger] : allEvents[trigger]
+    const listener = removeFlag ? 'removeEventListener' : 'addEventListener'
+    for (const e of events) {
+        element[listener](e, handler, capture)
+    }
+}
+function addListener(trigger, element, handler, capture = false) {
+    return EventListener({
+        trigger, element, handler, capture
+    })
+}
+function removeListener(trigger, element, handler, capture = false) {
+    return EventListener({
+        trigger, element, handler, capture, removeFlag: true
+    })
+}
+
+export {
+    triggerEvents,
+    addListener,
+    removeListener,
+    
+}
---
+++ ease-popup/src/index.js
@@ -0,0 +1,111 @@
+import { getxAxis, getyAxis, getDirection } from "./direction"
+import { resolveEl, resolveRect, resolveArrow, resolveOption, addStylesheetRules } from "./resolve"
+import { popupStyle } from "./option"
+
+class Popup {
+    constructor(target, popup, options = {}) {
+        if (!target || !popup) {
+            return
+        }
+
+        this.popup = resolveEl(popup)
+        this.target = resolveEl(target)
+        this.options = options
+
+        addStylesheetRules([popupStyle.popup], 'ease-popup')
+
+        if (this.options.needArrow) {
+            addStylesheetRules([popupStyle.arrow], 'ease-popup-arrow')
+        }
+
+        if (this.options.useCache) {
+            this.state = 0
+            this.styles = {}
+        }
+
+        this.applyStyle(
+            this.computeStyle()
+        )
+
+    }
+
+    computeStyle() {
+
+        if (this.options.useCache) {
+
+            console.log('useCache')
+
+            if (this.state >= 2) return this.styles
+
+            this.state++
+
+        }
+
+        const { direction, maxWidth, needArrow, offset, gap, arrowSize, background, color } = resolveOption(this.options)
+
+        const targetRect = resolveRect(this.target)
+
+        const popupRect = resolveRect(this.popup, maxWidth, offset.offsetX)
+
+        const offsetOptions = { offset, gap, arrowSize }
+
+        const resolvedDir = getDirection(targetRect, popupRect, direction, offsetOptions)
+
+        const { popupX, width, arrowX } = getxAxis(targetRect, popupRect, resolvedDir, offsetOptions)
+
+        const { popupY, arrowY } = getyAxis(targetRect, popupRect, resolvedDir, offsetOptions)
+
+        const styles = {
+            '--popup-x': `${popupX}px`,
+            '--popup-y': `${popupY}px`,
+            '--popup-width': `${width}px`,
+            '--popup-background': `${background}`,
+            '--popup-color': `${color}`
+        }
+
+        if (needArrow) {
+            styles['--arrow-x'] = `${arrowX}px`
+            styles['--arrow-y'] = `${arrowY}px`
+            styles['--arrow-size'] = `${arrowSize}px`
+            styles['--arrow-rotate'] = `${resolveArrow(resolvedDir)}deg`
+        }
+
+        this.options.direction = resolvedDir
+
+        return styles
+    }
+
+    applyStyle(styles) {
+        for (let key in styles) {
+            this.popup.style.setProperty(key, styles[key])
+        }
+
+        if (this.styles) this.styles = styles
+
+        return styles
+    }
+
+    update() {
+
+        this.applyStyle(
+            this.computeStyle()
+        )
+
+    }
+
+    show() {
+        this.update()
+        this.popup.style.display = 'block'
+        
+    }
+
+    hide() {
+
+        this.popup.style.display = 'none'
+    }
+
+}
+
+export {
+    Popup
+}
